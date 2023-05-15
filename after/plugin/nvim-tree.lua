5bbe297ff90cccb9d53586a971eb0d2d93a2f77c
Helehelehele <Helehelehele@users.noreply.github.com>
2023-05-15 12:28:53 +0300

    fix: Change nvim-tree auto close to use QuitPre event

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -27,15 +27,24 @@ require("nvim-tree").setup({
 
 vim.api.nvim_set_keymap("n", "<leader>o", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-    nested = true,
+-- Close nvim-tree when quitting
+-- Taken from https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#ppwwyyxx
+vim.api.nvim_create_autocmd("QuitPre", {
     callback = function()
-        if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-            vim.cmd "quit"
+        local invalid_win = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+            if bufname:match("NvimTree_") ~= nil then
+                table.insert(invalid_win, w)
+            end
+        end
+        if #invalid_win == #wins - 1 then
+            -- Should quit, so we close all invalid windows.
+            for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
         end
     end
 })
-
 
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function(data)
